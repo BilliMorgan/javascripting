bc0df68221038c4521110b3885f55ff3fa953a00
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-11-19 15:01:34 -0500

    second solution for sum largest numbers is done

--- SumNumbers.js
+++ SumNumbers.js
@@ -1,21 +1,30 @@
-const sumLargestNumbers = function (data) {
-  let secondNumber = 0;
-  let firstNumber = 0;
-  for (let i = 0; i < data.length; i++) {
-    if (data[i] > data[i + 1] && data[i] > firstNumber) {
-      firstNumber = data[i];
-    } else if (data[i] < data[i + 1] && data[i + 1] > firstNumber) {
-      firstNumber = data[i + 1];
-    }
-    for (let i = 0; i < data.length; i++) {
-      if (data[i] < firstNumber && data[i] > secondNumber) {
-        secondNumber = data[i];
-      }
-    }
-  }
+// const sumLargestNumbers = function (data) {
+//   let secondNumber = 0;
+//   let firstNumber = 0;
+//   for (let i = 0; i < data.length; i++) {
+//     if (data[i] > data[i + 1] && data[i] > firstNumber) {
+//       firstNumber = data[i];
+//     } else if (data[i] < data[i + 1] && data[i + 1] > firstNumber) {
+//       firstNumber = data[i + 1];
+//     }
+//     for (let i = 0; i < data.length; i++) {
+//       if (data[i] < firstNumber && data[i] > secondNumber) {
+//         secondNumber = data[i];
+//       }
+//     }
+//   }
+//   return firstNumber + secondNumber;
+// };
 
-  return firstNumber + secondNumber;
-};
+sumLargestNumbers = (data) => {
+  arr = data.sort((a, b) => { return a - b;})
+  // console.log(arr)
+  .slice(-2)
+  // console.log(arr)
+  .reduce((a,b) => {return a + b})
+  return arr;
+}
+
 
 console.log(sumLargestNumbers([1, 10]));
 console.log(sumLargestNumbers([1, 2, 3]));
